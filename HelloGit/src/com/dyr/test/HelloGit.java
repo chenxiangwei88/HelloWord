2ef6815b45ed3b8d9b16efe97850cc6f5f31603c
chenxiangwei88 <1032981913@qq.com>
2015-03-10 16:46:33 +0800

    增加了一条语句

--- HelloGit/src/com/dyr/test/HelloGit.java
+++ HelloGit/src/com/dyr/test/HelloGit.java
@@ -7,6 +7,8 @@ package com.dyr.test;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!!");
+		System.out.println("增加了一条语句");
+		System.out.println("再增加了一条语句");
 	}
 
 }
