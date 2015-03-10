550693bad8011b434e0816d27eef22ccebe0894d
chenxiangwei88 <1032981913@qq.com>
2015-03-10 16:43:38 +0800

    第二次commit

---
+++ HelloGit/src/com/dyr/test/HelloGit.java
@@ -0,0 +1,12 @@
+/*
+ *www.dyr.com
+ *Copyright (c) 2014 All Rights Reserved.
+ */
+package com.dyr.test;
+
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!!");
+	}
+
+}
